f8fa7b800ad863f29723cb8d0023e0899fa66a9e
Jazaltron <jazaltron10@gmail.com>
2022-02-09 23:19:03 +0100

    Adding SillySentences to the mix

--- src/School_Two/Week_2Two/Linked_List.java
+++ src/School_Two/Week_2Two/Linked_List.java
@@ -1,5 +1,18 @@
 package Week_2Two;
-public class Linked_List{
+
+public class Linked_List {
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    private static Node headNode;
+
     public static void main(String[] args) {
         System.out.println("Hello");
     }
