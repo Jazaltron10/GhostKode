47921944a27f50a129e7200f94d2481909f720ae
Jazaltron <jazaltron10@gmail.com>
2022-02-20 20:00:15 +0100

    Starting Out Week 4

---
+++ src/School_Two/Week_4Four/DGenerics.java
@@ -0,0 +1,6 @@
+package Week_4Four;
+public class DGenerics {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/School_Two/Week_4Four/Lab_8/ExercisesFour.java
@@ -0,0 +1,7 @@
+package Week_4Four.Lab_8;
+
+public class ExercisesFour {
+    public static void main(String[] args) {
+        System.out.println("x");
+    }
+}
