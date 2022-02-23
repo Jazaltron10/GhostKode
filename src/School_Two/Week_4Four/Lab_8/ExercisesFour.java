75dea2533df797c51a132fd81afeb7ec2039fe5f
Jazaltron <jazaltron10@gmail.com>
2022-02-23 19:28:09 +0100

    Wrapping up Week 4

--- src/School_Two/Week_4Four/Lab_8/ExercisesFour.java
+++ src/School_Two/Week_4Four/Lab_8/ExercisesFour.java
@@ -2,6 +2,11 @@ package Week_4Four.Lab_8;
 
 public class ExercisesFour {
     public static void main(String[] args) {
-        System.out.println("x");
+        Integer j = 45;
+        Integer a = 45;
+
+        if (j.equals(a)){
+            System.out.println("x");
+        }
     }
 }
