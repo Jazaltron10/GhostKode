af4b634abe3e5f8009729f8f2a50254ec48e2bdb
Jazaltron <jazaltron10@gmail.com>
2022-07-03 18:50:06 +0100

    Adding TwoNumberSum to the blind 75 QuestionSet

--- src/DataStructures/Blind75/blindArrays.java
+++ src/DataStructures/Blind75/blindArrays.java
@@ -1,7 +1,36 @@
-public class blindArrays {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
-        System.out.println("Welcome to Blind75's Java Snippets");
+public class blindArrays {
+    /**
+     * This variation of two number sum uses the concepts of two pointers to solve
+     * the question.
+     */
+    public static int[] TwoSum(int[] arr, int targetSum) {
+        // Write your code here.
+
+        Arrays.sort(arr);
+        int lhs = 0;
+        int rhs = arr.length - 1;
+        while (lhs < rhs) {
+            int sum = arr[lhs] + arr[rhs];
+            if (sum == targetSum) {
+                return new int[] { arr[lhs], arr[rhs] };
+            } else if (sum < targetSum) {
+                lhs++;
+            } else {
+                rhs--;
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] { 8, 9, 1, 2, 4, 3, 2, 3, 2, 2 };
+        int res[] = new int[a.length];
+        res = TwoSum(a, 9);
+        System.out.println(Arrays.toString(res));
+
+        // System.out.println("Welcome to Blind75's Java Snippets");
         // # Question 1
         // Two Sum
 
